54995a9836bda447de79e81941b9d1e3e44e1526
daws11 <dawskutel@gmail.com>
2025-04-30 14:06:35 +0700

    update baseURL config to heroku server @api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api/',
+  baseURL: 'https://jobseeker-company-4586a3f22963.herokuapp.com/api/',
   headers: {
     'Content-Type': 'application/json',
   },
